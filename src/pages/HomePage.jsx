0753e21d22c3adb3fd43ae10d054c3206f98a2e1
Bartlomiej Filipiuk <01169277@pw.edu.pl>
2023-12-18 02:17:19 +0100

    homepage tresci wyglad

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar/Navbar";
+import img_bmw from "../assets/bmw.png"
 import "./HomePage.css"
 
 const HomePage = () => {
@@ -9,12 +10,23 @@ const HomePage = () => {
         <div>
             <Navbar />
             <Header />
+        
+            <div className="flex-container-1">
+                <div className="left-1">
+                    <h1 className="dlaczego-my-h">Dlaczego my?</h1>
 
-            <h1 className="dlaczego-my-h">Dlaczego my?</h1>
-            <div className="textContainer1">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <div className="text-container-1">
+                        <p className="text-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                </div>
+
+                <div className="img-container-1">
+                    <img src = {img_bmw} alt="bmw img" />
+                </div>
             </div>
+
+            
             <Footer />
         </div>
     );
