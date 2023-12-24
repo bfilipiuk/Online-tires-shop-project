8d136036fcfc71dafa0b87a725ce69990536f63e
Bartlomiej Filipiuk <01169277@pw.edu.pl>
2023-12-24 15:50:58 +0100

    warning fixed

--- src/pages/Oferta.jsx
+++ src/pages/Oferta.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Button from "@mui/material/Button";
-import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
 import "./Oferta.css"
