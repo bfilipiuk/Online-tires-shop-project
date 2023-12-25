f456fda666d01b1de5e44e842fcc38854d83f25d
karolkapron <karolkapron11@gmail.com>
2023-12-25 15:17:05 +0100

    add test case

--- customer-service/src/test/resources/customer/insert-customer.sql
+++ customer-service/src/test/resources/customer/insert-customer.sql
@@ -1,3 +1,4 @@
 INSERT INTO Customer (customer_Id,first_name, second_name, email, login, password_col, active_flag, status_flag)
 VALUES
-(1, 'John', 'Doe', 'johndoe@example.com', 'johndoe', 'hashed_password_here', '1', '1');
+(1, 'John', 'Doe', 'johndoe@example.com', 'johndoe', 'hashed_password_here', '1', '1'),
+(2, 'John', 'B', 'johndoe@example.com', 'johndoe', 'hashed_password_here', '0', '1');
